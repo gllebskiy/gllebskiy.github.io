e6fed19647ed97c9ff01d50f4b178a0127c20cee
gllebskiy <47176922+gllebskiy@users.noreply.github.com>
2019-02-08 16:33:17 +0200

    Custom & Private Label Shaker Bottles

---
+++ Shaker Bottles/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+    
+    // Slider
+      $('.shakers-slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+      	infinite: false,
+      	prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-double-left"></i></button>',
+      	nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-double-right"></i></button>',
+      });
+      	
+});
